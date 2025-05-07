03e07d79ef8f79c3fa81d66da83770f25e233cd5
Yash Gotrijiya <95033275+YashG92@users.noreply.github.com>
2025-05-07 11:22:45 +0530

    added svgs and constant path

---
+++ lib/constants/assets_constants.dart
@@ -0,0 +1,11 @@
+class AssetsConstants {
+
+
+  ///Light Svgs
+  static const String _svgPathLight = 'assets/svgs/light';
+  static const String lightAppLogo = '$_svgPathLight/light_twitter_logo.svg';
+
+  ///Dark Svgs
+  static const String _svgPathDark = 'assets/svgs/dark';
+  static const String dartAppLogo = '$_svgPathDark/dark_twitter_logo.svg';
+}
