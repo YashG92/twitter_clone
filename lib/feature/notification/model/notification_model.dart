5edd708521380ddb033d59698b997c7469069f8e
Yash Gotrijiya <95033275+YashG92@users.noreply.github.com>
2025-05-28 14:55:42 +0530

    update in notification model

--- lib/feature/notification/model/notification_model.dart
+++ lib/feature/notification/model/notification_model.dart
@@ -2,6 +2,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 enum NotificationType { like, reply, retweet, follow }
 
+NotificationType getNotificationType(String type) {
+  switch (type) {
+    case 'like':
+      return NotificationType.like;
+    case 'reply':
+      return NotificationType.reply;
+    case 'retweet':
+      return NotificationType.retweet;
+    case 'follow':
+      return NotificationType.follow;
+    default:
+      return NotificationType.like;
+  }
+}
+
+String notificationTypeToString(NotificationType type) {
+  return type.toString().split('.').last;
+}
+
 class NotificationModel {
   final String notificationId;
   final String userId;
